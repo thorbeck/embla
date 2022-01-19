0947dabecf7663c7a340214e741387ddc3d8e6b2
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-19 09:35:32 +0100

    Firebase init

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'embla-2f0a9',
+    appId: '1:425909760633:web:a82977e6c43c971f729aa8',
+    storageBucket: 'embla-2f0a9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyACN3kj_QTrhn40h0rKZYFMj7ygNvjuiJc',
+    authDomain: 'embla-2f0a9.firebaseapp.com',
+    messagingSenderId: '425909760633',
+  },
   production: true
 };
