7ddd241af049d55c4bc32c08782a499c4c617b1c
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-20 19:40:14 +0100

    Init navigation

--- app/src/app/core/navigation/navigation.component.ts
+++ app/src/app/core/navigation/navigation.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
 })
-export class NavigationComponent implements OnInit {
-  constructor() {}
+export class NavigationComponent {
+  elements: any[] | null = null;
+  components: any[] | null = null;
 
-  ngOnInit(): void {}
+  constructor(private dataService: DataService) {
+    // Get elements
+    dataService.getCollection('elements').then((data: any) => {
+      this.elements = data;
+    });
+    // Get components
+    dataService.getCollection('components').then((data: any) => {
+      this.components = data;
+    });
+  }
 }
