abc53670c421b989d47c82c251e2892c53f0c40f
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-20 19:40:05 +0100

    Removed oninit

--- app/src/app/shared/page/page.component.ts
+++ app/src/app/shared/page/page.component.ts
@@ -10,16 +10,14 @@ import { IPage } from './page.interface';
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
 })
-export class PageComponent implements OnInit {
-  item$: IPage | null = null;
+export class PageComponent {
+  item: IPage | null = null;
   constructor(private dataService: DataService, private router: Router) {
     dataService.getPage(router.url.replace('/', '')).then((data: any) => {
-      this.item$ = {
+      this.item = {
         title: data.title,
         content: data.content,
       };
     });
   }
-
-  ngOnInit(): void {}
 }
