cc0922fa3271008c33ea0c57d717a8d6b11bf23e
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-21 04:29:21 +0100

    gh-pages config

--- app/gh-pages.js
+++ app/gh-pages.js
@@ -1,10 +1,10 @@
 import { publish } from "gh-pages";
 
 publish(
-  "build", // path to public directory
+  "dist", // path to public directory
   {
     branch: "gh-pages",
-    repo: "https://github.com/thorbeck/rafflinator.git", // Update to point to your repository
+    repo: "https://github.com/thorbeck/embla.git", // Update to point to your repository
     user: {
       name: "Tom-Erik Thorbeck", // update to use your name
       email: "tomerik@thorbeck.no", // Update to use your email
@@ -12,6 +12,6 @@ publish(
     dotfiles: true,
   },
   () => {
-    console.log("Deploy Complete!");
+    console.log("Deploy successful!");
   }
 );
