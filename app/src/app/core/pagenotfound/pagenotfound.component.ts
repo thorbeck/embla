6881864ce39c688a4ac396c670cd64c0224b1771
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-22 23:36:31 +0100

    Page not found component

---
+++ app/src/app/core/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenotfound',
+  templateUrl: './pagenotfound.component.html',
+})
+export class PagenotfoundComponent {
+  constructor() {}
+}
