a780b173f5f8781def0484e8df00301269f977b5
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-22 23:37:15 +0100

    Detail view navigation

--- app/src/app/core/navigation/navigation.module.ts
+++ app/src/app/core/navigation/navigation.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationComponent } from './navigation.component';
 import { RouterModule } from '@angular/router';
+
+import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   declarations: [NavigationComponent],
