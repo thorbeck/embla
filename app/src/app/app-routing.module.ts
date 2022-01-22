ccbd90bc34583f55f88c1c50f14a8198b39d83df
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-01-22 23:38:13 +0100

    Routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './core/pagenotfound/pagenotfound.component';
 import { CustomRoutes } from './core/routes';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('./features/page/page.module').then((m) => m.PageModule),
+  },
+  {
     path: CustomRoutes.elements,
-    loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
+    loadChildren: () =>
+      import('./features/page/page.module').then((m) => m.PageModule),
+  },
+  {
+    path: CustomRoutes.elements + '/:tag',
+    loadChildren: () =>
+      import('./features/detail/detail.module').then((m) => m.DetailModule),
   },
   {
     path: CustomRoutes.components,
-    loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
+    loadChildren: () =>
+      import('./features/page/page.module').then((m) => m.PageModule),
   },
+  {
+    path: CustomRoutes.components + '/:tag',
+    loadChildren: () =>
+      import('./features/detail/detail.module').then((m) => m.DetailModule),
+  },
+  { path: '404', component: PagenotfoundComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '404' },
 ];
 
 @NgModule({
--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnsureImportedOnceModule } from './import-guard';
 import { NavigationModule } from './navigation/navigation.module';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PagenotfoundComponent],
   imports: [CommonModule, NavigationModule],
   exports: [NavigationModule],
 })
