ba1aae7441f3a1558bf2d3498ee50b03a9ccd5e9
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-10-31 11:36:59 +0100

    Moved angular project

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,16 +1,23 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import { setupCounter } from './counter'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-import { defineCustomElements } from '@abytegray/embla/dist/web-components/loader';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
-defineCustomElements(window);
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
