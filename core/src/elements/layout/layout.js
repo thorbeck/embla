86450843c1dd67fb4d149abead0573f2254058db
Tom-Erik Thorbeck <Tomerik@thorbeck.no>
2023-03-01 14:43:43 +0100

    Layout

--- core/src/elements/layout/layout.js
+++ core/src/elements/layout/layout.js
@@ -1,19 +1,44 @@
 import styles from "./layout.module.scss";
-import EmblaEvents from "../../events.js";
+// import EmblaEvents from "../../events.js";
 
 export class EmblaLayout extends HTMLElement {
   constructor() {
     super();
+    // Set up state
+    this.state = {
+      event: null,
+      grid: null,
+    };
+  }
+
+  // props
+  static get observedAttributes() {
+    // return all data attributes as an array
+    return ["data-event", "data-grid"];
+  }
+
+  // prop change
+  attributeChangedCallback(name, oldValue, newValue) {
+    // remove data- from name
+    name = name.replace("data-", "");
+
+    // check if value has changed
+    if (oldValue !== newValue) {
+      // update state with new value
+      this.state[name] = newValue;
+    }
   }
 
   connectedCallback() {
     this.classList.add(styles.layout);
     this.classList.add(styles[this.dataset.grid]);
 
-    // Listen for nav toggle event
-    this.addEventListener(EmblaEvents.navToggle, (e) => {
+    // Listen for event
+    this.addEventListener(this.state.event, (e) => {
       let aside = this.querySelector("aside");
       aside.classList.toggle(styles.open);
     });
   }
 }
+
+customElements.define("embla-layout", EmblaLayout);
--- core/src/index.js
+++ core/src/index.js
@@ -5,3 +5,6 @@ import "./css/normalize.scss";
 
 // Extended elements
 export { EmblaButton } from "./elements/_button/_button.js";
+
+// Elements
+export { EmblaLayout } from "./elements/layout/layout.js";
